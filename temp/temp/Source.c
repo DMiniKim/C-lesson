c0d1dbe02d1d5165d1855c22619a18e015faf97b
DMiniKim <angks1212@naver.com>
2025-06-04 02:06:54 +0900

    (2025.6.3) 메모리 영역_2

--- temp/temp/Source.c
+++ temp/temp/Source.c
@@ -7,11 +7,11 @@ int main()
 	// 프로그램을 실행 중에 필요한 만큼 메모리를 할당하는 
 	// 작업입니다.
 
-	int* point = (int*)malloc(sizeof(int));
-	
-	*point = 10;
-
-	printf("%d\n", *point);
+	//	int* point = (int*)malloc(sizeof(int));
+	//	
+	//	*point = 10;
+	//	
+	//	printf("%d\n", *point);
 
 
 	// 동적 할당은 실행 시간에 가변적으로 메모리의 크기를
@@ -35,9 +35,42 @@ int main()
 				// 사용이 끝나면 직접 해제해줘야 한다.
 
 #pragma region UAF Use After Free
-	// UAF 는 Heap 영역에서 할당되 malloc 공간을 free로
-	// 해제하고, 다시 메모리를 할당 시 
-	// 같은 공간을 재사용하면서 생기는 취약점을 말한다.
+	// UAF( Use After Free)란 , Heap메모리를 할당한 후 
+	// 해제한 뒤에도 해당 포인터를 계속 사용할 때 발생 되는 문제다.
+	
+	// free() 후 포인터를 NULL로 초기화 하지 않으면,
+	// 그 주소가 그대로 남아있기 때문에 다른 코드에서
+	// 해당 영역을 다시 할당받아버리거나
+	// 의도치 않게 접근 하게 되면서 프로그램 오류나,
+	// 함수의 예측하지 못한 동작 , 심하게는 보안적으로 위험해진다.
+	
+	// 따라서 할당과 해제를 명확하게 구분하고,
+	// 반드시 해당 포인터는 NULL 처리 하는 습관이 필요하다.
+
+	// ex )
+
+	int* point = (int*)malloc(sizeof(int));
+	printf("point 의 주소는 %p\n", point);
+
+	*point = 10;
+	printf("point 의 값은 %d\n", *point);
+
+	free(point);
+	printf("point 의 주소는 %p\n", point);
+
+	printf("=====================\n");
+
+	point = (int*)malloc(sizeof(int)); // 재 할당
+	printf("point 의 주소는 %p\n", point);
+
+	*point = 20;
+	printf("point 의 값은 %d\n", *point);
+
+	free(point);
+
+	printf("=====================\n");
+	point = (int*)malloc(sizeof(int*)*10); // 재 할당
+	printf("point 의 주소는 %p\n", point);
 #pragma endregion
 
 #pragma endregion
